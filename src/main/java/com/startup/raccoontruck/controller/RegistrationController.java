1037696a064ad6f63ec77657bde8ce7997cacdf4
“Vlad <vlad.atsman@gmail.com>
2020-03-30 09:59:53 +0300

    add Admin role and check rights by `hasAuthority`

--- src/main/java/com/startup/raccoontruck/controller/RegistrationController.java
+++ src/main/java/com/startup/raccoontruck/controller/RegistrationController.java
@@ -6,6 +6,7 @@ import com.startup.raccoontruck.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collections;
@@ -17,7 +18,7 @@ public class RegistrationController {
     private UserRepo userRepo;
 
     @GetMapping("/registration")
-    public String registration() {
+    public String registration(@ModelAttribute("message") String message ) {
         return "registration";
     }
 
