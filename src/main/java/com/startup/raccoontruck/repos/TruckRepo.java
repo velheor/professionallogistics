61eabdc659c9ff9df022f52b4adf68ba0be9b8ba
“Vlad <vlad.atsman@gmail.com>
2020-05-02 11:40:16 +0300

    delete trucks

--- src/main/java/com/startup/raccoontruck/repos/TruckRepo.java
+++
@@ -1,9 +0,0 @@
-package com.startup.raccoontruck.repos;
-
-import com.startup.raccoontruck.domain.Truck;
-
-import java.util.List;
-
-public interface TruckRepo {
-    List<Truck> findByUser(String userName);
-}
