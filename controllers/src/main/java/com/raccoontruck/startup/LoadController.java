4b05831b41c72fa216a146998cb7b01b809c412d
“Vlad <vlad.atsman@gmail.com>
2020-11-20 01:21:30 +0300

    clean

--- controllers/src/main/java/com/raccoontruck/startup/LoadController.java
+++ controllers/src/main/java/com/raccoontruck/startup/LoadController.java
@@ -1,23 +1,50 @@
 package com.raccoontruck.startup;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/load")
 public class LoadController {
     private final ILoadService loadService;
 
     @Autowired
-    LoadController(ILoadService loadService) {
-        this.loadService = loadService;
+    LoadController(ILoadService LoadService) {
+        this.loadService = LoadService;
     }
 
-    @GetMapping("/loads")
-    public List<LoadDTO> findAll() {
+    @GetMapping
+    public List<LoadDTO> getAll() {
         return loadService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public LoadDTO getById(@PathVariable("id") Long id) {
+        return loadService.findById(id);
+    }
+
+    @DeleteMapping
+    public LoadDTO delete(@RequestBody LoadDTO loadDTO) {
+        loadService.delete(loadDTO.getId());
+        return loadDTO;
+    }
+
+    @PutMapping
+    public LoadDTO update(@RequestBody LoadDTO loadDTO) {
+        return loadService.update(loadDTO);
+    }
+
+    @PostMapping
+    public LoadDTO save(@RequestBody LoadDTO loadDTO) {
+        return loadService.update(loadDTO);
     }
 }
