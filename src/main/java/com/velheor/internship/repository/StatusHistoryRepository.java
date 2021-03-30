b1ed9248d9e1dc17540d636ed5b781d739abd6ba
Uladzislau Atsman <vlad.atsman@gmail.com>
2021-03-30 16:53:04 +0300

    Uuid to integer

--- src/main/java/com/velheor/internship/repository/StatusHistoryRepository.java
+++ src/main/java/com/velheor/internship/repository/StatusHistoryRepository.java
@@ -2,9 +2,10 @@ package com.velheor.internship.repository;
 
 import com.velheor.internship.models.StatusHistory;
 import java.util.List;
+import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 
-public interface StatusHistoryRepository extends CrudRepository<StatusHistory, Integer> {
+public interface StatusHistoryRepository extends CrudRepository<StatusHistory, UUID> {
 
     @Override
     List<StatusHistory> findAll();
--- src/main/java/com/velheor/internship/service/api/ILoadCategoryService.java
+++ src/main/java/com/velheor/internship/service/api/ILoadCategoryService.java
@@ -1,9 +1,8 @@
 package com.velheor.internship.service.api;
 
 import com.velheor.internship.models.LoadCategory;
-import java.util.UUID;
 
 public interface ILoadCategoryService extends Crud<LoadCategory> {
 
-    LoadCategory findById(UUID id);
+    LoadCategory findById(Integer id);
 }
