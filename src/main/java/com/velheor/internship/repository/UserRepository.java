f9d3f9b85b2feb0192d782957fb3ca49bd2e6a9b
Uladzislau Atsman <vlad.atsman@gmail.com>
2021-03-30 15:37:19 +0300

    added method

--- src/main/java/com/velheor/internship/repository/UserRepository.java
+++ src/main/java/com/velheor/internship/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.velheor.internship.repository;
 
 import com.velheor.internship.models.User;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 
+public interface UserRepository extends CrudRepository<User, UUID> {
 
-public interface UserRepository extends CrudRepository<User, Long> {
+    @Override
+    List<User> findAll();
 
     User findByEmail(String email);
 }
