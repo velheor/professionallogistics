4cebb32b0bd5d589451a16c4b565368ea8d848e8
Uladzislau Atsman <vlad.atsman@gmail.com>
2021-03-30 17:02:49 +0300

    user to optional return type findByEmail

--- src/main/java/com/velheor/internship/repository/UserRepository.java
+++ src/main/java/com/velheor/internship/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.velheor.internship.repository;
 
 import com.velheor.internship.models.User;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 
@@ -10,5 +11,5 @@ public interface UserRepository extends CrudRepository<User, UUID> {
     @Override
     List<User> findAll();
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
