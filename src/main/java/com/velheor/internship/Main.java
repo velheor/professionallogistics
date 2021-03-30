949b82d448e2914db448aba2f1e03b0009010a86
Uladzislau Atsman <vlad.atsman@gmail.com>
2021-03-30 17:43:59 +0300

    service test

--- src/main/java/com/velheor/internship/Main.java
+++ src/main/java/com/velheor/internship/Main.java
@@ -1,18 +1,18 @@
 package com.velheor.internship;
 
-import com.velheor.internship.conf.PersistenceConfig;
-import com.velheor.internship.repository.UserRepository;
+import com.velheor.internship.service.impl.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.velheor.internship")
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(PersistenceConfig.class);
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(Main.class);
 
-
-        UserRepository userRepository = ctx.getBean(UserRepository.class);
-        System.out.println(userRepository.findByEmail("123").getFirstName());
+        UserService userService = ctx.getBean(UserService.class);
+        System.out.println(userService.findByEmail("iv@gmail.com").getFirstName());
     }
 
 }
