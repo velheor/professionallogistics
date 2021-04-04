11c2a779562e0cee04907e1f14a82f9d964d2d6e
Uladzislau Atsman <vlad.atsman@gmail.com>
2021-04-05 02:26:51 +0300

    delete iloadservice

--- src/main/java/com/velheor/internship/service/api/ILoadCategoryService.java
+++
@@ -1,8 +0,0 @@
-package com.velheor.internship.service.api;
-
-import com.velheor.internship.models.LoadCategory;
-
-public interface ILoadCategoryService extends Crud<LoadCategory> {
-
-    LoadCategory findById(Integer id);
-}
