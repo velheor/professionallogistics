ae0ce2e614406553188e961539c16be770370f2e
Uladzislau Atsman <vlad.atsman@gmail.com>
2021-04-05 18:01:50 +0300

    added new status

--- src/main/java/com/velheor/internship/models/enums/EStatusHistory.java
+++ src/main/java/com/velheor/internship/models/enums/EStatusHistory.java
@@ -1,5 +1,5 @@
 package com.velheor.internship.models.enums;
 
 public enum EStatusHistory {
-    WAITING_FOR_SHIPPER, WAITING_FOR_LOADING, ON_WAY, ENDED, CANCELED
+    WAITING_FOR_SHIPPER, WAITING_FOR_LOADING, ON_WAY, ENDED, CANCELED, STARTED
 }
