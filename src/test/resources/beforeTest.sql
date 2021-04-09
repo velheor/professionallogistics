99a3bf184ec3c2052559d73316cdb77bf389ba3e
Uladzislau Atsman <vlad.atsman@gmail.com>
2021-04-09 14:10:20 +0300

    delete prolog.

--- src/test/resources/beforeTest.sql
+++ src/test/resources/beforeTest.sql
@@ -1,27 +1,27 @@
-TRUNCATE TABLE prolog.users;
-TRUNCATE TABLE prolog.trucks_categories;
-TRUNCATE TABLE prolog.trucks;
-TRUNCATE TABLE prolog.orders;
-TRUNCATE TABLE prolog.status_history;
-TRUNCATE TABLE prolog.orders_address;
-TRUNCATE TABLE prolog.loads;
-TRUNCATE TABLE prolog.users_has_orders;
+TRUNCATE TABLE users;
+TRUNCATE TABLE trucks_categories;
+TRUNCATE TABLE trucks;
+TRUNCATE TABLE orders;
+TRUNCATE TABLE status_history;
+TRUNCATE TABLE orders_address;
+TRUNCATE TABLE loads;
+TRUNCATE TABLE users_has_orders;
 
-INSERT INTO prolog.users
+INSERT INTO users
 VALUES ('47a07384-93b8-11eb-a8b3-0242ac130003', 'Ivan', 'Ivanov', 'ivan@gmail.com', '+375331234567',
         'pass1', 'CARRIER', 'a0a81b2e-9725-11eb-a8b3-0242ac130003'),
        ('45caf4c2-9565-11eb-a8b3-0242ac130003', 'Petr', 'Petrov', 'petr@gmail.com', '+375296888258',
         'pass2', 'CARRIER', null);
 
-INSERT INTO prolog.trucks_categories
+INSERT INTO trucks_categories
 VALUES (0, 'COVERED', null),
        (1, 'ALL-METAL', 0);
 
-INSERT INTO prolog.trucks
+INSERT INTO trucks
 VALUES ('a0a81b2e-9725-11eb-a8b3-0242ac130003', 'VOLVO', '1234VA-5', 10, 1),
        ('886c0c76-9727-11eb-a8b3-0242ac130003', 'SCANIA', '2345AV-6', 11, 1);
 
-INSERT INTO prolog.orders
+INSERT INTO orders
 VALUES ('377514cc-958b-11eb-a8b3-0242ac130003',
         parsedatetime('2021.01.04 11:30', 'yyyy.MM.dd hh:mm'),
         parsedatetime('2021.01.10 10:00', 'yyyy.MM.dd hh:mm'), 4000, ''),
@@ -30,7 +30,7 @@ VALUES ('377514cc-958b-11eb-a8b3-0242ac130003',
         parsedatetime('2021.02.10 12:00', 'yyyy.MM.dd hh:mm'),
         parsedatetime('2021.02.12 06:00', 'yyyy.MM.dd hh:mm'), 5000, '');
 
-INSERT INTO prolog.status_history
+INSERT INTO status_history
 VALUES ('377514cc-958b-11eb-a8b3-0242ac130003', 'STARTED',
         parsedatetime('2021.01.04 11:30', 'yyyy.MM.dd hh:mm'),
         '377514cc-958b-11eb-a8b3-0242ac130003'),
@@ -39,24 +39,24 @@ VALUES ('377514cc-958b-11eb-a8b3-0242ac130003', 'STARTED',
         parsedatetime('2021.01.10 10:00', 'yyyy.MM.dd hh:mm'),
         '377514cc-958b-11eb-a8b3-0242ac130003');
 
-INSERT INTO prolog.orders_address
+INSERT INTO orders_address
 VALUES ('a12ee7be-9589-11eb-a8b3-0242ac130003', 'HRODNO', 'MINSK',
         '377514cc-958b-11eb-a8b3-0242ac130003'),
 
        ('a678774e-9589-11eb-a8b3-0242ac130003', 'VITEBSK', 'BREST',
         '3a424170-958b-11eb-a8b3-0242ac130003');
 
-INSERT INTO prolog.loads
+INSERT INTO loads
 VALUES ('5942070a-957b-11eb-a8b3-0242ac130003', 'FURNITURE', 0.5, 'Just furniture',
         '377514cc-958b-11eb-a8b3-0242ac130003'),
 
        ('60b523b4-957b-11eb-a8b3-0242ac130003', 'BEER', 23, 'HEINEKEN',
         '3a424170-958b-11eb-a8b3-0242ac130003');
 
-INSERT INTO prolog.users_has_orders
+INSERT INTO users_has_orders
 VALUES ('47a07384-93b8-11eb-a8b3-0242ac130003', '377514cc-958b-11eb-a8b3-0242ac130003'),
        ('45caf4c2-9565-11eb-a8b3-0242ac130003', '377514cc-958b-11eb-a8b3-0242ac130003');
 
-INSERT INTO prolog.orders_has_trucks_categories
+INSERT INTO orders_has_trucks_categories
 VALUES ('377514cc-958b-11eb-a8b3-0242ac130003', 1),
        ('3a424170-958b-11eb-a8b3-0242ac130003', 1);
