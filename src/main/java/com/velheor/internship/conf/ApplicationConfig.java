2bad40ea8cc0288c84d356c795e495b759b820e8
Uladzislau Atsman <vlad.atsman@gmail.com>
2021-04-14 14:37:25 +0300

    conf for web mvc

--- src/main/java/com/velheor/internship/conf/ApplicationConfig.java
+++ src/main/java/com/velheor/internship/conf/ApplicationConfig.java
@@ -1,12 +1,16 @@
 package com.velheor.internship.conf;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan("com.velheor.internship")
-@Import({PersistenceConfig.class, LiquiBaseConfig.class})
+@Import({PersistenceConfig.class, LiquiBaseConfig.class, WebConfig.class})
+@RequiredArgsConstructor
 public class ApplicationConfig {
 
+    private final ApplicationContext applicationContext;
 }
---
+++ src/main/java/com/velheor/internship/conf/MySpringMvcDispatcherSerlvetIntitializer.java
@@ -0,0 +1,21 @@
+package com.velheor.internship.conf;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{ApplicationConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
