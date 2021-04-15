7f35c00c043095543eee363411a4dcd9aa3575d4
Uladzislau Atsman <vlad.atsman@gmail.com>
2021-04-15 13:40:49 +0300

    validation

--- src/main/java/com/velheor/internship/dto/TruckDTO.java
+++ src/main/java/com/velheor/internship/dto/TruckDTO.java
@@ -2,15 +2,18 @@ package com.velheor.internship.dto;
 
 import java.math.BigDecimal;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class TruckDTO extends BaseDTO {
 
     @NotNull(message = "{notEmpty}")
+    @Size(min = 2, max = 12, message = "notCorrectSize}")
     private String name;
 
     @NotNull(message = "{notEmpty}")
+    @Size(min = 9, max = 9, message = "notCorrectSize}")
     private String registrationNumber;
 
     @NotNull(message = "{notEmpty}")
--- src/main/java/com/velheor/internship/dto/UserDTO.java
+++ src/main/java/com/velheor/internship/dto/UserDTO.java
@@ -3,22 +3,28 @@ package com.velheor.internship.dto;
 import java.util.List;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserDTO extends BaseDTO {
 
-    @NotNull(message = "{notEmpty}")
+    @NotNull(message = "notEmpty")
+    @Size(min = 2, max = 12, message = "notCorrectSize}")
     private String firstName;
 
-    @NotNull(message = "{notEmpty}")
+    @NotNull(message = "notEmpty")
+    @Size(min = 2, max = 12, message = "notCorrectSize")
     private String lastName;
 
-    @NotNull(message = "{notEmpty}")
-    @Email(message = "{validation}")
+    @NotNull(message = "notEmpty")
+    @Email
     private String email;
 
-    @NotNull(message = "{notEmpty}")
+    @NotNull(message = "notEmpty")
+    @Size(min = 12, max = 12, message = "notCorrectSize")
+    @Pattern(regexp = "(8 0(25|29|33|34) ([0-9]{3}( [0-9]{2}){2}))")
     private String phoneNumber;
 
     private List<RoleDTO> roles;
