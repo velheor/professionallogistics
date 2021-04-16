e81cf28c31e5e19c775099d4be47cb91d1f4f946
Uladzislau Atsman <vlad.atsman@gmail.com>
2021-04-16 13:18:52 +0300

    updated exception

--- src/main/java/com/velheor/internship/exception/ErrorMessage.java
+++ src/main/java/com/velheor/internship/exception/ErrorMessage.java
@@ -1,11 +1,10 @@
 package com.velheor.internship.exception;
 
-import java.time.LocalDateTime;
 import lombok.Data;
 
 @Data
 public class ErrorMessage {
 
-    private final LocalDateTime timestamp;
+    private final String timestamp;
     private final String message;
 }
--- src/main/java/com/velheor/internship/exception/RestExceptionHandler.java
+++ src/main/java/com/velheor/internship/exception/RestExceptionHandler.java
@@ -3,16 +3,19 @@ package com.velheor.internship.exception;
 import java.time.LocalDateTime;
 import javax.persistence.EntityNotFoundException;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Log4j2
 @RestControllerAdvice
 public class RestExceptionHandler {
 
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
     @ExceptionHandler(EntityNotFoundException.class)
     public ErrorMessage entityNotFoundException(EntityNotFoundException ex) {
         log.info(ex.getMessage());
-        return new ErrorMessage(LocalDateTime.now(), ex.getMessage());
+        return new ErrorMessage(LocalDateTime.now().toString(), ex.getMessage());
     }
 }
