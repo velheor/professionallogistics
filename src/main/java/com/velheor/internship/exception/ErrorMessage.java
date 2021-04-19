d3cd3b765bbc4ec620a3e49e5b86908caf6cd71b
Uladzislau Atsman <vlad.atsman@gmail.com>
2021-04-19 11:40:05 +0300

    updated

--- src/main/java/com/velheor/internship/exception/ErrorMessage.java
+++ src/main/java/com/velheor/internship/exception/ErrorMessage.java
@@ -1,10 +1,24 @@
 package com.velheor.internship.exception;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 
 @Data
 public class ErrorMessage {
 
-    private final String timestamp;
+    private final HttpStatus status;
     private final String message;
+    private final List<String> errors;
+    private final LocalDateTime timestamp;
+
+    public ErrorMessage(HttpStatus status, String message, String errors,
+        LocalDateTime timestamp) {
+        this.status = status;
+        this.message = message;
+        this.errors = Arrays.asList(errors);
+        this.timestamp = timestamp;
+    }
 }
--- src/main/java/com/velheor/internship/exception/RestExceptionHandler.java
+++ src/main/java/com/velheor/internship/exception/RestExceptionHandler.java
@@ -2,13 +2,13 @@ package com.velheor.internship.exception;
 
 import java.time.LocalDateTime;
 import javax.persistence.EntityNotFoundException;
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Log4j2
+@Slf4j
 @RestControllerAdvice
 public class RestExceptionHandler {
 
@@ -16,6 +16,7 @@ public class RestExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ErrorMessage entityNotFoundException(EntityNotFoundException ex) {
         log.info(ex.getMessage());
-        return new ErrorMessage(LocalDateTime.now().toString(), ex.getMessage());
+        return new ErrorMessage(HttpStatus.NOT_FOUND, ex.getMessage(), ex.toString(),
+            LocalDateTime.now());
     }
 }
