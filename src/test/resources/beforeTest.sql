46a6618ae46b6aba7e9e2197e9b548b5616cc53c
Uladzislau Atsman <vlad.atsman@gmail.com>
2021-04-27 16:12:47 +0300

    repair sql sqript for password encode

--- src/test/resources/beforeTest.sql
+++ src/test/resources/beforeTest.sql
@@ -8,10 +8,12 @@ TRUNCATE TABLE routes;
 TRUNCATE TABLE roles;
 
 INSERT INTO users
-VALUES ('47a07384-93b8-11eb-a8b3-0242ac130003', 'Ivan', 'Ivanov', 'ivan@gmail.com', '+375331234567',
-        'pass1'),
-       ('45caf4c2-9565-11eb-a8b3-0242ac130003', 'Petr', 'Petrov', 'petr@gmail.com', '+375296888258',
-        'pass2');
+VALUES ('47a07384-93b8-11eb-a8b3-0242ac130003', 'Test1', 'Test1', 'test1@gmail.com',
+        '+375 (33) 123-45-67',
+        '$2y$12$yVeTM63pz0oSJeet.BGEU.GxJvJdnf0FX5rGcqGl4Mk51edhBa1SC'),
+       ('45caf4c2-9565-11eb-a8b3-0242ac130003', 'Test2', 'Test2', 'test2@gmail.com',
+        '+375 (33) 111-22-33',
+        '$2y$12$ZN3OaMsgVkx9Z6.b.tnHSeok9zxUSVtQH9A0JFxSEhW8son/POBXi');
 
 INSERT INTO trucks
 VALUES ('2da16836-9c4a-11eb-a8b3-0242ac130003', 'VOLVO', '1234VA-5', 4000, 'ALL_METAL',
