0c49662a85afed108d32eaf83402fbdf1360d43f
Uladzislau Atsman <vlad.atsman@gmail.com>
2021-04-30 12:58:33 +0300

    added ResponseEntityExceptionHandler, return responseEntity with error message, header, status

--- src/main/java/com/velheor/internship/exception/ErrorMessage.java
+++ src/main/java/com/velheor/internship/exception/ErrorMessage.java
@@ -1,26 +1,23 @@
 package com.velheor.internship.exception;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.http.HttpStatus;
 
 @Data
 @AllArgsConstructor
 public class ErrorMessage {
 
-    private final HttpStatus status;
     private final String message;
     private final List<String> errors;
     private final LocalDateTime timestamp;
 
-    public ErrorMessage(HttpStatus status, String message, String errors,
+    public ErrorMessage(String message, String errors,
         LocalDateTime timestamp) {
-        this.status = status;
         this.message = message;
-        this.errors = Arrays.asList(errors);
+        this.errors = Collections.singletonList(errors);
         this.timestamp = timestamp;
     }
 }
--- src/main/java/com/velheor/internship/exception/RestExceptionHandler.java
+++ src/main/java/com/velheor/internship/exception/RestExceptionHandler.java
@@ -2,57 +2,74 @@ package com.velheor.internship.exception;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
 @RestControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     @ExceptionHandler(EntityNotFoundException.class)
-    public ErrorMessage entityNotFoundException(EntityNotFoundException ex) {
-        log.info(ex.getMessage());
-        return new ErrorMessage(HttpStatus.NOT_FOUND, ex.getMessage(), ex.toString(),
-            LocalDateTime.now());
+    public ResponseEntity<Object> entityNotFoundException(EntityNotFoundException ex,
+        HttpHeaders headers, HttpStatus status,
+        WebRequest request) {
+
+        return handleExceptionInternal(ex, new ErrorMessage(ex.getMessage(), ex.toString(),
+            LocalDateTime.now()), headers, status, request);
     }
 
+    @Override
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorMessage methodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        log.info(ex.getMessage());
+    public ResponseEntity<Object> handleMethodArgumentNotValid(
+        MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status,
+        WebRequest request) {
 
         List<String> errors = ex.getBindingResult().getAllErrors().stream()
             .filter(error -> error instanceof FieldError)
             .map(error -> (FieldError) error)
             .map(error -> error.getField() + " : " + error.getDefaultMessage())
             .collect(Collectors.toList());
-        return new ErrorMessage(HttpStatus.BAD_REQUEST, ex.getMessage(), errors,
-            LocalDateTime.now());
+
+        return handleExceptionInternal(ex, new ErrorMessage(ex.getMessage(), errors,
+            LocalDateTime.now()), headers, status, request);
     }
 
     @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(JwtAuthenticationException.class)
-    public ErrorMessage jwtAuthenticationException(JwtAuthenticationException ex) {
-        log.info(ex.getMessage());
-        return new ErrorMessage(HttpStatus.UNAUTHORIZED, ex.getMessage(), ex.toString(),
-            LocalDateTime.now());
+    public ResponseEntity<Object> jwtAuthenticationException(JwtAuthenticationException ex,
+        HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return handleExceptionInternal(ex, null, headers, status, request);
     }
 
     @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(BadCredentialsException.class)
-    public ErrorMessage badCredentialsException(BadCredentialsException ex) {
-        log.info(ex.getMessage());
-        return new ErrorMessage(HttpStatus.UNAUTHORIZED, ex.getMessage(),
-            "Incorrect email or password",
-            LocalDateTime.now());
+    public ResponseEntity<Object> badCredentialsException(BadCredentialsException ex,
+        HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return handleExceptionInternal(ex, null, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body,
+        HttpHeaders headers, HttpStatus status, WebRequest request) {
+        log.warn(ex.getMessage());
+        if (Objects.isNull(body)) {
+            body = new ErrorMessage(ex.getMessage(), ex.toString(),
+                LocalDateTime.now());
+        }
+        return super.handleExceptionInternal(ex, body, headers, status, request);
     }
 }
