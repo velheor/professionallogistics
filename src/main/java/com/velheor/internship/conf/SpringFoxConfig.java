664143e8e3615a6dd6f6b9f665f2e9e72236bc5c
Uladzislau Atsman <vlad.atsman@gmail.com>
2021-04-30 16:31:42 +0300

    enabled open api 3.0

--- src/main/java/com/velheor/internship/conf/SpringFoxConfig.java
+++ src/main/java/com/velheor/internship/conf/SpringFoxConfig.java
@@ -6,12 +6,12 @@ import org.springframework.context.annotation.Import;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
+@EnableOpenApi
 @Import(BeanValidatorPluginsConfiguration.class)
 public class SpringFoxConfig {
 
