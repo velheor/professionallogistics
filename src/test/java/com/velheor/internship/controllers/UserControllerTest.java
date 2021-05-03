8b6b32aecf7dcd8c805bbfa52c5e47bb35bb9b58
Uladzislau Atsman <vlad.atsman@gmail.com>
2021-05-03 13:21:44 +0300

    added test method for check no handleMethodArgumentNotValid

--- src/test/java/com/velheor/internship/controllers/UserControllerTest.java
+++ src/test/java/com/velheor/internship/controllers/UserControllerTest.java
@@ -4,6 +4,7 @@ import static com.velheor.internship.utils.TestUtils.USER1;
 import static com.velheor.internship.utils.TestUtils.USER2;
 import static com.velheor.internship.utils.TestWebUtils.USER_VIEW_DTO1;
 import static com.velheor.internship.utils.TestWebUtils.USER_VIEW_DTO2;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -16,12 +17,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.velheor.internship.BaseWebTest;
+import com.velheor.internship.dto.UserViewDTO;
 import com.velheor.internship.mappers.UserMapper;
 import com.velheor.internship.mappers.UserMapperImpl;
 import com.velheor.internship.service.UserService;
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 class UserControllerTest extends BaseWebTest {
 
@@ -75,6 +78,20 @@ class UserControllerTest extends BaseWebTest {
     }
 
     @Test
+    void updateThrowHandleMethodArgumentNotValid() throws Exception {
+        UserViewDTO testUser = new UserViewDTO(USER_VIEW_DTO1);
+        testUser.setPassword("test");
+        testUser.setPhoneNumber("+1234");
+        mockMvc.perform(put(user_url)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(testUser)))
+            .andExpect(status().isBadRequest())
+            .andExpect(result -> assertTrue(
+                result.getResolvedException() instanceof MethodArgumentNotValidException))
+            .andExpect(content().json("test"));
+    }
+
+    @Test
     void save() throws Exception {
         doReturn(USER1).when(userService).save(USER1);
         mockMvc.perform(post(user_url)
@@ -87,8 +104,8 @@ class UserControllerTest extends BaseWebTest {
 
     @Test
     void deleteById() throws Exception {
-        mockMvc.perform(delete(user_url + USER1.getId()))
-            .andExpect(status().isNoContent());
+        mockMvc.perform(delete(user_url + USER1.getId())).andExpect(status().isNoContent());
+
         verify(userService).deleteById(USER1.getId());
     }
 }
