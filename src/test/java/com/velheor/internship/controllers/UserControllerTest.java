ecddbaffd8e8169faee552737dae5efb9c1ab436
vladislav.atsman <vlad.atsman@gmail.com>
2021-05-05 18:03:58 +0300

    user controller test

--- src/test/java/com/velheor/internship/controllers/UserControllerTest.java
+++ src/test/java/com/velheor/internship/controllers/UserControllerTest.java
@@ -1,11 +1,25 @@
 package com.velheor.internship.controllers;
 
+import com.velheor.internship.BaseWebTest;
+import com.velheor.internship.dto.UserViewDTO;
+import com.velheor.internship.exception.ErrorMessage;
+import com.velheor.internship.mappers.UserMapper;
+import com.velheor.internship.mappers.UserMapperImpl;
+import com.velheor.internship.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Arrays;
+
 import static com.velheor.internship.utils.TestUtils.USER1;
 import static com.velheor.internship.utils.TestUtils.USER2;
 import static com.velheor.internship.utils.TestWebUtils.USER_VIEW_DTO1;
 import static com.velheor.internship.utils.TestWebUtils.USER_VIEW_DTO2;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -16,16 +30,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.velheor.internship.BaseWebTest;
-import com.velheor.internship.dto.UserViewDTO;
-import com.velheor.internship.mappers.UserMapper;
-import com.velheor.internship.mappers.UserMapperImpl;
-import com.velheor.internship.service.UserService;
-import java.util.Arrays;
-import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-
+@WithMockUser(authorities = "ADMIN")
 class UserControllerTest extends BaseWebTest {
 
     private final String user_url = "/users/";
@@ -45,19 +50,9 @@ class UserControllerTest extends BaseWebTest {
     void findById() throws Exception {
         when(userService.findById(USER1.getId())).thenReturn(USER1);
         mockMvc.perform(get(user_url + USER1.getId()))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(content().json(objectMapper.writeValueAsString(USER_VIEW_DTO1)));
-    }
-
-    @Test
-    void getAll() throws Exception {
-        when(userService.getAll()).thenReturn(Arrays.asList(USER1, USER2));
-        mockMvc.perform(get(user_url))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(content().json(
-                objectMapper.writeValueAsString(Arrays.asList(USER_VIEW_DTO1, USER_VIEW_DTO2))));
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(objectMapper.writeValueAsString(USER_VIEW_DTO1)));
     }
 
     @Test
@@ -66,15 +61,33 @@ class UserControllerTest extends BaseWebTest {
     }
 
     @Test
+    void findByIdNotExistsUser() throws Exception {
+        mockMvc.perform(get(user_url + "415ab4a6-adaf-11eb-8529-0242ac130003"))
+                .andExpect(status().isOk())
+                .andExpect(result -> assertTrue(
+                        result.getResolvedException() instanceof EntityNotFoundException));
+    }
+
+    @Test
+    void getAll() throws Exception {
+        when(userService.getAll()).thenReturn(Arrays.asList(USER1, USER2));
+        mockMvc.perform(get(user_url))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(
+                        objectMapper.writeValueAsString(Arrays.asList(USER_VIEW_DTO1, USER_VIEW_DTO2))));
+    }
+
+    @Test
     void update() throws Exception {
         when(userService.save(USER1)).thenReturn(USER1);
 
         mockMvc.perform(put(user_url)
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(USER_VIEW_DTO1)))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(content().json(objectMapper.writeValueAsString(USER_VIEW_DTO1)));
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(USER_VIEW_DTO1)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(objectMapper.writeValueAsString(USER_VIEW_DTO1)));
     }
 
     @Test
@@ -82,24 +95,28 @@ class UserControllerTest extends BaseWebTest {
         UserViewDTO testUser = new UserViewDTO(USER_VIEW_DTO1);
         testUser.setPassword("test");
         testUser.setPhoneNumber("+1234");
-        mockMvc.perform(put(user_url)
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(testUser)))
-            .andExpect(status().isBadRequest())
-            .andExpect(result -> assertTrue(
-                result.getResolvedException() instanceof MethodArgumentNotValidException))
-            .andExpect(content().json("test"));
+
+        String responseBody = mockMvc.perform(put(user_url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(testUser)))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(
+                        result.getResolvedException() instanceof MethodArgumentNotValidException))
+                .andReturn().getResponse().getContentAsString();
+        ErrorMessage actual = objectMapper.readValue(responseBody, ErrorMessage.class);
+        int countOfErrors = 2;
+        assertThat(actual.getErrors().size()).isEqualTo(countOfErrors);
     }
 
     @Test
     void save() throws Exception {
-        doReturn(USER1).when(userService).save(USER1);
+        when(userService.save(USER1)).thenReturn(USER1);
         mockMvc.perform(post(user_url)
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(USER_VIEW_DTO1)))
-            .andExpect(status().isCreated())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(content().json(objectMapper.writeValueAsString(USER_VIEW_DTO1)));
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(USER_VIEW_DTO1)))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(objectMapper.writeValueAsString(USER_VIEW_DTO1)));
     }
 
     @Test
