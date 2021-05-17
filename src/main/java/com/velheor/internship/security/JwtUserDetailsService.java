fc61c124acffe168b77222a9c6249a4b106950fc
vladislav.atsman <vlad.atsman@gmail.com>
2021-05-17 18:30:27 +0300

    deleted useless throw

--- src/main/java/com/velheor/internship/security/JwtUserDetailsService.java
+++ src/main/java/com/velheor/internship/security/JwtUserDetailsService.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,7 +15,8 @@ public class JwtUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) {
+
         User user = userRepository.findByEmail(username)
                 .orElseThrow(() -> new BadCredentialsException("email/password incorrect"));
 
