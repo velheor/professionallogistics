347f2e83ebeb0ca5baf5e2f688a3f216b26b9fe2
vladislav.atsman <vlad.atsman@gmail.com>
2021-05-19 12:26:00 +0300

    feature/prolog-1: added clone constructor in AuthUserDTO

--- src/main/java/com/velheor/internship/dto/AuthUserDTO.java
+++ src/main/java/com/velheor/internship/dto/AuthUserDTO.java
@@ -1,12 +1,14 @@
 package com.velheor.internship.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 @Data
+@NoArgsConstructor
 public class AuthUserDTO {
 
     @Email(message = "{notValidEmail}")
@@ -17,4 +19,9 @@ public class AuthUserDTO {
     @NotEmpty(message = "{notEmpty}")
     @Size(min = 7, max = 255, message = "{notCorrectSize}")
     private String password;
+
+    public AuthUserDTO(AuthUserDTO authUserDTO) {
+        this.email = authUserDTO.getEmail();
+        this.password = authUserDTO.getPassword();
+    }
 }
