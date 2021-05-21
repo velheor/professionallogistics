c059a5c53ed051534f90b7c934692ce082dc6a51
vladislav.atsman <vlad.atsman@gmail.com>
2021-05-21 16:25:43 +0300

    Prolog-1: Feature: migrated from easyRandom to javaFaker

--- src/test/java/com/velheor/internship/utils/RandomDataInPojo.java
+++ src/test/java/com/velheor/internship/utils/RandomDataInPojo.java
@@ -1,10 +1,9 @@
 package com.velheor.internship.utils;
 
-import com.velheor.internship.models.User;
+import com.github.javafaker.Faker;
+import com.velheor.internship.dto.UserViewDTO;
 import com.velheor.internship.xml.customModels.Users;
-import org.jeasy.random.EasyRandom;
-import org.jeasy.random.EasyRandomParameters;
-import org.jeasy.random.FieldPredicates;
+import org.junit.jupiter.api.Test;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -16,28 +15,34 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class RandomDataInPojo {
+
+    @Test
+    void randomData() throws JAXBException, FileNotFoundException {
+        creatingTestData();
+    }
+
     public void creatingTestData() throws JAXBException, FileNotFoundException {
         JAXBContext context = JAXBContext.newInstance(Users.class);
 
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(marshaller.JAXB_FORMATTED_OUTPUT, true);
         marshaller.setProperty(marshaller.JAXB_ENCODING, "utf-8");
-        List<User> userList = new ArrayList<>();
+        List<UserViewDTO> userList = new ArrayList<>();
         Users users = new Users();
+        Faker faker = new Faker();
 
-        EasyRandomParameters parameters = new EasyRandomParameters();
-
-        parameters.excludeField(FieldPredicates.named("carrierOrders"));
-        parameters.excludeField(FieldPredicates.named("shipperOrders"));
-        parameters.excludeField(FieldPredicates.named("roles"));
-        parameters.excludeField(FieldPredicates.named("truck"));
-
-        EasyRandom easyRandom = new EasyRandom(parameters);
-
-        for (int i = 0; i < 10000; i++) {
-            userList.add(easyRandom.nextObject(User.class));
+        for (int i = 0; i < 50000; i++) {
+            UserViewDTO user = new UserViewDTO();
+            user.setId(UUID.randomUUID());
+            user.setFirstName(faker.name().firstName());
+            user.setLastName(faker.name().lastName());
+            user.setEmail(faker.bothify("????##@gmail.com"));
+            user.setPhoneNumber(faker.regexify("\\+375 \\((17|29|33|44)\\) [0-9]{3}-[0-9]{2}-[0-9]{2}"));
+            user.setPassword(faker.internet().password(8, 20));
+            userList.add(user);
         }
 
         users.setUsers(userList);
